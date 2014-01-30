79cfc251b8e5425ecd252eacc2cf83b2391f4607
Gabor Szabo <szabgab@gmail.com>
2014-01-30 10:08:36 +0200

    working on the next step

--- bin/mboxer.pl
+++ bin/mboxer.pl
@@ -4,15 +4,18 @@ use 5.010;
 
 use Path::Iterator::Rule;
 use Email::Folder;
-#use MongoDB;
+use Email::Address;
+use MongoDB;
 use Data::Dumper qw(Dumper);
 
 my $path_to_dir = shift or die "Usage: $0 path/to/mail\n";
 
-#my $client     = MongoDB::MongoClient->new(host => 'localhost', port => 27017);
-#my $database   = $client->get_database( 'mboxer');
-#my $collection = $database->get_collection( 'messages' );
-#
+my $client     = MongoDB::MongoClient->new(host => 'localhost', port => 27017);
+my $database   = $client->get_database( 'mboxer');
+my $collection = $database->get_collection( 'messages' );
+#$collection->remove;
+
+
 
 my $count = 0;
 
@@ -27,21 +30,35 @@ while ( my $file = $it->() ) {
 		#say $msg->header;
 		# Use of uninitialized value $field in lc at .../5.18.1/Email/Simple/Header.pm line 123, <GEN0> line 14.
 		#say $msg->header('From');
-		#Email::Address->parse($line);
-		foreach my $h ($msg->headers) {
-			$main::count{$h}++;
+		my @from = Email::Address->parse($msg->header('From'));
+		if (@from > 1) {
+			warn "Stranage, there were more than one emails recognized in the From field: " . $msg->header('From');
 		}
-		#exit if $count > 20;
-		#exit if $main::cnt++ > 40;
+		if (not @from) {
+			warn "Very strange. No email in the From field! " . $msg->header('From');
+			next;
+		}
+		#say Dumper \@from;
+		say $from[0]->address;
+		say $from[0]->name;
+		if ($from[0]->name eq 'Mail System Internal Data') {
+			next;
+		}
+		#exit;
+		my %doc = (
+			file => $file,
+			From => {
+				name => $from[0]->name,
+				address => $from[0]->address,
+			}
+		);
+		$collection->insert(\%doc);
+		exit if $count > 20;
 	}
-	last;
+	#last;
 	#exit;
 }
 say $count;
-foreach my $k (sort keys %main::count) {
-	say "$k  $main::count{$k}";
-}
-#print Dumper \%main::count;
 
 
 
